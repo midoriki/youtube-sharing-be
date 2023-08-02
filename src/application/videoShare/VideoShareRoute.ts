0b5d9c299963c1f4e56fbad5cfae368002f8656e
Thanh Nguyen <thanh@midoriki.com>
2023-08-03 04:58:19 +0700

    fix: added authorized route to get all videoshares

--- src/application/videoShare/VideoShareRoute.ts
+++ src/application/videoShare/VideoShareRoute.ts
@@ -5,7 +5,8 @@ import { authenticated } from '@libs/auth/strategies/JwtStrategy';
 
 const route = express.Router();
 
-route.get('/', queryValidator('pagination'), all);
+route.get('/all', queryValidator('pagination'), all);
+route.get('/', [authenticated, queryValidator('pagination')], all);
 route.post('/', [authenticated, bodyValidator('createVideoShare')], create);
 
 export default route;
